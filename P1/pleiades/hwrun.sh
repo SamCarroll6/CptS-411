33529fc949533f34a282e4687b2605efb1726252
Carroll <samuel.carroll@pleiades.tricity.wsu.edu>
2019-09-11 12:31:31 -0700

    Updating pleiades file for Project 1 specifications.

--- P1/pleiades/hwrun.sh
+++ P1/pleiades/hwrun.sh
@@ -4,14 +4,20 @@ if [ -z "$1" ]
 then
 	mpicc -o main ~/CptS-411/P1/pleiades/main.c
 else
-	mpicc -o $1 ~/CptS-411/P1/pleiades/main.c
+	mpicc -o $1 ~/CptS-411/P1/pleiades/$1.c
 fi
 
 if [ -z "$2"]
 then
-	mpirun -np 4 ~/CptS-411/P1/pleiades/main
+	mpirun -np 2 ~/CptS-411/P1/pleiades/main
 else
 	mpirun -np $2 ~/CptS-411/P1/pleiades/$1
 fi
 
+if [ -z "$1" ]
+then
+	rm main
+else
+	rm $1
+fi
 
---
+++ P1/pleiades/nonblockmain.c
@@ -0,0 +1,72 @@
+/*	Cpt S 411, Introduction to Parallel Computing
+ *	School of Electrical Engineering and Computer Science
+ *	
+ *	Example code
+ *	Send receive test:
+ *   	rank 1 sends to rank 0 (all other ranks sit idle)
+ *   	For timing use of C gettimeofday() is recommended.
+ * */
+
+
+#include <stdio.h>
+#include <mpi.h> 
+#include <assert.h>
+#include <sys/time.h>
+
+int main(int argc,char *argv[])
+{
+
+   int rank,p;
+   struct timeval t1,t2;
+
+   MPI_Init(&argc,&argv);
+   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+   MPI_Comm_size(MPI_COMM_WORLD,&p);
+
+   printf("my rank=%d\n",rank);
+   printf("Rank=%d: number of processes =%d\n",rank,p);
+
+   assert(p>=2);
+
+
+    if(rank==1) {
+		char *x = "a";
+		int dest = 0;
+		gettimeofday(&t1,NULL);
+		MPI_Send(&x,1,MPI_CHAR,dest,0,MPI_COMM_WORLD);
+		gettimeofday(&t2,NULL);
+		int tSend = (t2.tv_sec-t1.tv_sec)*1000 + (t2.tv_usec-t1.tv_usec)/1000;
+
+		printf("Rank=%d: sent message %s to rank %d; Send time %d millisec\n", rank,x,dest,tSend);
+   } 
+   else if (rank==0) {
+   		char *y="\0";
+		MPI_Status status;
+		gettimeofday(&t1,NULL);
+   		MPI_Recv(&y,1,MPI_CHAR,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+		gettimeofday(&t2,NULL);
+		int tRecv = (t2.tv_sec-t1.tv_sec)*1000 + (t2.tv_usec-t1.tv_usec)/1000;
+		printf("Rank=%d: received message %s from rank %d; Recv time %d millisec\n",rank,y,status.MPI_SOURCE,tRecv);
+   }
+   else if(rank==3) {
+		char *x = "AbcdefghijklmnopqrstuvwxyZ";
+		int dest = 2;
+		gettimeofday(&t1,NULL);
+		MPI_Send(&x,1,MPI_CHAR,dest,0,MPI_COMM_WORLD);
+		gettimeofday(&t2,NULL);
+		int tSend = (t2.tv_sec-t1.tv_sec)*1000 + (t2.tv_usec-t1.tv_usec)/1000;
+
+		printf("Rank=%d: sent message %s to rank %d; Send time %d millisec\n", rank,x,dest,tSend);
+   } 
+   else if (rank==2) {
+   		char *y="\0";
+		MPI_Status status;
+		gettimeofday(&t1,NULL);
+   		MPI_Recv(&y,1,MPI_CHAR,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+		gettimeofday(&t2,NULL);
+		int tRecv = (t2.tv_sec-t1.tv_sec)*1000 + (t2.tv_usec-t1.tv_usec)/1000;
+		printf("Rank=%d: received message %s from rank %d; Recv time %d millisec\n",rank,y,status.MPI_SOURCE,tRecv);
+   }
+
+   MPI_Finalize();
+}
