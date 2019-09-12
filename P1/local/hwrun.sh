5fcb9d2ec6f2a62137ba0c3ff99f0da2945dbaf2
Sam Carroll <cfcarroll321@gmail.com>
2019-09-11 22:25:37 -0700

    Got nonblocking and blocking both working. Updated run script.

--- P1/local/hwrun.sh
+++ P1/local/hwrun.sh
@@ -4,7 +4,7 @@ if [ -z "$1" ]
 then
 	mpicc -o main ~/CptS-411/P1/pleiades/main.c
 else
-	mpicc -o $1 ~/CptS-411/P1/pleiades/$1.c
+	mpicc -o $1 ./$1.c
 fi
 
 if [ -z "$2"]
@@ -14,11 +14,11 @@ then
 		mpirun -np 2 ~/CptS-411/P1/pleiades/main
 		rm main
 	else
-		mpirun -np 2 ~/CptS-411/P1/pleiades/$1
+		mpirun -np 2 ./$1
 		rm $1
 	fi
 else
-	mpirun -np $2 ~/CptS-411/P1/pleiades/$1
+	mpirun -np $2 ./$1
 	rm $1
 fi
 
