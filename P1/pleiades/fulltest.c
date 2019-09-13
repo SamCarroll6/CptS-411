4788e93c0b053c0a16b26952f2e790fdd959d0fb
Sam Carroll <cfcarroll321@gmail.com>
2019-09-12 20:40:40 -0700

    Updating test somewhat.

--- P1/pleiades/fulltest.c
+++ P1/pleiades/fulltest.c
@@ -13,7 +13,8 @@
 #include <assert.h>
 #include <sys/time.h>
 
-#define MB 1000000
+//#define MB 1000000
+#define MB 512
 
 int main(int argc,char *argv[])
 {
@@ -32,7 +33,7 @@ int main(int argc,char *argv[])
 
 
     if(rank==1) {
-		char x[MB] = {'0'};
+		char x[MB] = {'a'};
 		int dest = 0;
         int i = 0;
         for(i = 1; i < (MB + 1); i *= 2)
@@ -40,12 +41,12 @@ int main(int argc,char *argv[])
             gettimeofday(&t1,NULL);
             MPI_Send(&x,i,MPI_CHAR,dest,0,MPI_COMM_WORLD);
             gettimeofday(&t2,NULL);
-            int tSend = (t2.tv_sec-t1.tv_sec) + (t2.tv_usec-t1.tv_usec);
-            printf("Rank=%d: sent message %s to rank %d; Send time %d millisec\n", rank,x,dest,tSend);
+            int tSend = (t2.tv_sec-t1.tv_sec) + (t2.tv_usec-t1.tv_usec)/1000;
+            printf("Rank=%d: sent message %s to rank %d; Send time %d millisec\n", rank,"bullshit",dest,tSend);
         }
    } 
    else if (rank==0) {
-   		char y[MB]={'0'};
+   		char y[MB]={'a'};
 		MPI_Status status;
         MPI_Request request;
         int i;
@@ -55,12 +56,12 @@ int main(int argc,char *argv[])
             MPI_Irecv(&y,i,MPI_CHAR,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&request);
             MPI_Wait(&request, &status);
             gettimeofday(&t2,NULL);
-            int tRecv = (t2.tv_sec-t1.tv_sec) + (t2.tv_usec-t1.tv_usec);
+            int tRecv = (t2.tv_sec-t1.tv_sec) + (t2.tv_usec-t1.tv_usec)/1000;
             printf("Rank=%d: received message %s from rank %d; Recv time %d millisec\n",rank,y,status.MPI_SOURCE,tRecv);
         }
    }
    else if(rank==3) {
-        char x[MB] = {'0'};
+        char x[MB] = {'a'};
 		int dest = 2;
         int i;
         for(i = 1; i < (MB + 1); i *=2)
@@ -69,11 +70,11 @@ int main(int argc,char *argv[])
             MPI_Send(&x,200,MPI_BYTE,dest,0,MPI_COMM_WORLD);
             gettimeofday(&t2,NULL);
             int tSend = (t2.tv_sec-t1.tv_sec)*1000 + (t2.tv_usec-t1.tv_usec)/1000;
-            printf("Rank=%d: sent message %s to rank %d; Send time %d millisec\n", rank,x,dest,tSend);
+            printf("Rank=%d: sent as message %s to rank %d; Send time %d millisec\n", rank,x,dest,tSend);
         }
    } 
    else if (rank==2) {
-   		char y[MB]={'0'};
+   		char y[MB]={'a'};
 		MPI_Status status;
         int i;
         for(i = 1; i < (MB + 1); i *=2)
