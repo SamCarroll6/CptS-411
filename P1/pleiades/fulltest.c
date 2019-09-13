9010e7888df3195fb00bf682044847ae359c0905
Sam Carroll <cfcarroll321@gmail.com>
2019-09-12 23:45:13 -0700

    Added comments, seems like a final product, will do write up tomorrow.

--- P1/pleiades/fulltest.c
+++ P1/pleiades/fulltest.c
@@ -1,11 +1,23 @@
-/*	Cpt S 411, Introduction to Parallel Computing
- *	School of Electrical Engineering and Computer Science
- *	
- *	Example code
- *	Send receive test:
- *   	rank 1 sends to rank 0 (all other ranks sit idle)
- *   	For timing use of C gettimeofday() is recommended.
- * */
+/*	
+ * Samuel Carroll
+ * CPTS 411
+ * HW 1
+ * September 17, 2019
+ *
+ * The first two if statements represent the nonblocking test section. It will be relevant on 
+ * ranks 1 and 0. Rank 1 will do a regular send of the message, which will increment in size each
+ * iteration up until it hits one megabyte. Rank 0 will be receiving the message
+ * from rank 1 using the MPI_Irecv command which is non blocking. It uses the MPI_Wait()
+ * command after to wait until the message has arrived and then read it. This isn't that
+ * far off from a blocking call since I call wait immediately after the receive call, however
+ * it does use this alternative, and since I'm not actually doing anything else there's no reason
+ * to use MPI_Test to do other things while waiting for the message. Thus MPI_Wait was the choice.
+ *
+ * This next two if statements are for the blocking call, it simply uses MPI_Send in rank 3, 
+ * and MPI_Recv in rank 2. It prints out the time taken to process the send and receive in
+ * milliseconds. Just like the non blocking test it iterates it's bytes sent/received until it
+ * gets to one MB at which point it exits and finalizes.
+ */
 
 
 #include <stdio.h>
@@ -13,6 +25,7 @@
 #include <assert.h>
 #include <sys/time.h>
 
+// First value over 1 mb that is hit from doubling each iteration (since it doesn't come out to exactly 1MB)
 #define MB 1048576
 
 int main(int argc,char *argv[])
@@ -30,17 +43,18 @@ int main(int argc,char *argv[])
 
    assert(p>=2);
 
-
     if(rank==1) {
+        // Initialize send buffer
 		char x[MB] = {'a'};
 		int dest = 0;
         int i = 0;
         for(i = 1; i < (MB + 1); i *= 2)
         {
-            memset(x,'a',sizeof(char)*(i-1));
+            //memset(x,'a',sizeof(char)*(i-1));
             gettimeofday(&t1,NULL);
             MPI_Send(&x,i,MPI_CHAR,dest,0,MPI_COMM_WORLD);
             gettimeofday(&t2,NULL);
+            // Returns time in milliseconds. 
             double tSend = (t2.tv_sec-t1.tv_sec)*1000000 + (double)(t2.tv_usec-t1.tv_usec)/1000;
             printf("Rank=%d: sent of %d bytes to rank %d; Send time %lf millisec\n",rank,i,dest,tSend);
         }
@@ -54,6 +68,7 @@ int main(int argc,char *argv[])
         {
             gettimeofday(&t1,NULL);
             MPI_Irecv(&y,i,MPI_CHAR,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&request);
+            // Use MPI_Wait to stop everything until message is received. 
             MPI_Wait(&request, &status);
             gettimeofday(&t2,NULL);
             double tRecv = (t2.tv_sec-t1.tv_sec)*1000000 + (double)(t2.tv_usec-t1.tv_usec)/1000;
@@ -66,7 +81,7 @@ int main(int argc,char *argv[])
         int i;
         for(i = 1; i < (MB + 1); i *=2)
         {
-            memset(x,'a',sizeof(char)*(i-1));
+            //memset(x,'a',sizeof(char)*(i-1));
             gettimeofday(&t1,NULL);
             MPI_Send(&x,i,MPI_BYTE,dest,0,MPI_COMM_WORLD);
             gettimeofday(&t2,NULL);
