2da2607a53e654ae842c77b5958d8eb8e5534f89
Sam Carroll <cfcarroll321@gmail.com>
2019-09-30 15:01:15 -0700

    Finished local code, uploading pleidas code to test on cluster.

--- P2/pleiades/hwrun.sh
+++ P2/pleiades/hwrun.sh
@@ -4,23 +4,41 @@ set -e
 
 if [ -z "$1" ]
 then
-	mpicc -o main ~/CptS-411/P1/pleiades/main.c
+	mpicc -o main ~/CptS-411/P2/pleiades/main.c
 else
-	mpicc -o $1 ~/CptS-411/P1/pleiades/$1.c
+	mpicc -o $1 ~/CptS-411/P2/pleiades/$1.c
 fi
 
-if [ -z "$2"]
+
+if [ -z "$3" ]
 then
-	if [ -z "$1" ]
+	if [ -z "$2" ]
 	then
-		mpirun -np 2 ~/CptS-411/P1/pleiades/main
-		rm main
+		if [ -z "$1" ]
+		then
+			mpirun -np 2 ~/CptS-411/P2/pleiades/main 8
+			rm main
+		else
+			mpirun -np 2 ./$1 8
+			rm $1
+		fi
 	else
-		mpirun -np 2 ~/CptS-411/P1/pleiades/$1
+		mpirun -np $2 ./$1 8
 		rm $1
 	fi
 else
-	mpirun -np $2 ~/CptS-411/P1/pleiades/$1
-	rm $1
+	if [ -z "$2" ]
+	then
+		if [ -z "$1" ]
+		then
+			mpirun -np 2 mpirun -np 2 ~/CptS-411/P2/pleiades/main $3
+			rm main
+		else
+			mpirun -np 2 ./$1 $3
+			rm $1
+		fi
+	else
+		mpirun -np $2 ./$1 $3
+		rm $1
+	fi
 fi
-
