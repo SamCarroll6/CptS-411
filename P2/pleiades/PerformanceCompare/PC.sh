40b1108e74c3dd9d67318c559fb5b6ea57e97f43
Sam Carroll <cfcarroll321@gmail.com>
2019-10-02 13:17:03 -0700

    Adding sleep command to run script, had issue with multiple nodes trying to access the executable simultaneously.

--- P2/pleiades/PerformanceCompare/PC.sh
+++ P2/pleiades/PerformanceCompare/PC.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 sbatch -N$1 ./hwrun.sh $2 $3 1000 1
-
+sleep 1
 sbatch -N$1 ./hwrun.sh $2 $3 2000 1
-
+sleep 1
 sbatch -N$1 ./hwrun.sh $2 $3 4000 1
-
+sleep 1
 sbatch -N$1 ./hwrun.sh $2 $3 8000 1
-
+sleep 1
 sbatch -N$1 ./hwrun.sh $2 $3 16000 1
-
+sleep 1
 sbatch -N$1 ./hwrun.sh $2 $3 32000 1
-
+sleep 1
 sbatch -N$1 ./hwrun.sh $2 $3 64000 1
-
+sleep 1
 sbatch -N$1 ./hwrun.sh $2 $3 640000 1
