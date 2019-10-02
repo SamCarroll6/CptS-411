a30d98b46e44fd8ee13f82c51b369fba2e85127b
Sam Carroll <cfcarroll321@gmail.com>
2019-10-01 17:37:15 -0700

    Added script for correctness testing up to 640000 values checking for max, using up to 64 processes.

---
+++ P2/local/CT.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+./hwrun.sh main 1 1000 1
+
+./hwrun.sh main 2 2000 1
+
+./hwrun.sh main 4 4000 1 
+
+./hwrun.sh main 8 8000 1
+
+./hwrun.sh main 16 16000 1
+
+./hwrun.sh main 32 32000 1
+
+./hwrun.sh main 64 64000 1
---
+++ P2/pleiades/CT.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sbatch -N1 ./hwrun.sh main 1 1000 1
+
+sbatch -N2 ./hwrun.sh main 2 2000 1
+
+sbatch -N2 ./hwrun.sh main 4 4000 1
+
+sbatch -N4 ./hwrun.sh main 8 8000 1
+
+sbatch -N8 ./hwrun.sh main 16 16000 1
+
+sbatch -N8 ./hwrun.sh main 32 32000 1
+
+sbatch -N8 ./hwrun.sh main 64 64000 1
+
+sbatch -N8 ./hwrun.sh main 64 640000 1
