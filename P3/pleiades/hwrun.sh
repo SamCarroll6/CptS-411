10bdf94ef5223673de4ae2449a015bbd8607b233
Sam Carroll <cfcarroll321@gmail.com>
2019-11-04 16:19:08 -0800

    Updating run script for cluster.

--- P3/pleiades/hwrun.sh
+++ P3/pleiades/hwrun.sh
@@ -4,9 +4,9 @@ set -e
 
 if [ -z "$1" ]
 then
-	mpicc -o main ~/School/CptS411/CptS-411/P3/local/main.c
+	mpicc -o main ./main.c
 else
 	mpicc -o $1 ./$1.c
 fi
 
-mpirun -np 8 ~/School/CptS411/CptS-411/P3/local/main 15 37 24 9241 20000
+mpirun -np 8 ./main 15 37 24 9241 20000
