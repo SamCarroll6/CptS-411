cc2e702e1b41467f050bb43919aa24c6587001b8
Sam Carroll <cfcarroll321@gmail.com>
2019-11-04 16:25:12 -0800

    Hardcoding to test.

--- P3/pleiades/main.c
+++ P3/pleiades/main.c
@@ -113,7 +113,8 @@ void parallelPrefix(int M_loc[2][2], int p, int rank, int Prime)
     int l[2][2], g[2][2], g_remote[2][2];
     MPI_Status status;
     int i = 0, mate;
-    int log2p = (log2(p) / log2(2));
+   // int log2p = (log2(p) / log2(2));
+   int log2p = 3;
     // l[0][0] = 1;
     // l[0][1] = 0;
     // l[1][0] = 0;
