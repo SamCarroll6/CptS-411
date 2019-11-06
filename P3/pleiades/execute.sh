fa8afe857d4f093b2dad5dc62310cb0239715aeb
Sam Carroll <cfcarroll321@gmail.com>
2019-11-05 18:45:16 -0800

    Fixing execute script.

--- P3/pleiades/execute.sh
+++ P3/pleiades/execute.sh
@@ -17,51 +17,58 @@
 
 # p seed A B Prime n
 
-if [ -z "$1" ]
-then
-	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
-else
-	sbatch -N$1 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
-fi
-
-if [ -z "$2" ]
-then
-	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
-else
-	sbatch -N$1 ./hwrun.sh $2 150 74 49 49999 1024 | sort -V
-fi
-
-if [ -z "$3" ]
-then
-	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
-else
-	sbatch -N$1 ./hwrun.sh $2 $3 74 49 49999 1024 | sort -V
-fi
-
-if [ -z "$4" ]
-then
-	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
-else
-	sbatch -N$1 ./hwrun.sh $2 $3 $4 49 49999 1024 | sort -V
-fi
-
-if [ -z "$5" ]
-then
-	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
-else
-	sbatch -N$1 ./hwrun.sh $2 $3 $4 $5 49999 1024 | sort -V
-fi
-
-if [ -z "$6" ]
-then
-	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
-else
-	sbatch -N$1 ./hwrun.sh $2 $3 $4 $5 $6 1024 | sort -V
-fi
-
 if [ -z "$7" ]
 then
 	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
 else
 	sbatch -N$1 ./hwrun.sh $2 $3 $4 $5 $6 $7 | sort -V
 fi
+
+# elif [ -z "$6" ]
+# then
+# 	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+# else
+# 	sbatch -N$1 ./hwrun.sh $2 $3 $4 $5 $6 1024 | sort -V
+# fi
+
+# elif [ -z "$5" ]
+# then
+# 	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+# else
+# 	sbatch -N$1 ./hwrun.sh $2 $3 $4 $5 49999 1024 | sort -V
+# fi
+
+# elif [ -z "$4" ]
+# then
+# 	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+# else
+# 	sbatch -N$1 ./hwrun.sh $2 $3 $4 49 49999 1024 | sort -V
+# fi
+
+# elif [ -z "$3" ]
+# then
+# 	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+# else
+# 	sbatch -N$1 ./hwrun.sh $2 $3 74 49 49999 1024 | sort -V
+# fi
+
+# elif [ -z "$2" ]
+# then
+# 	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+# else
+# 	sbatch -N$1 ./hwrun.sh $2 150 74 49 49999 1024 | sort -V
+# fi
+
+# elif [ -z "$1" ]
+# then
+# 	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+# else
+# 	sbatch -N$1 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+# fi
+
+
+
+
+
+
+
