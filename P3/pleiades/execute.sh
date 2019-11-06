a58dc43be9631c7baaaf1e06391181436d172c33
Sam Carroll <cfcarroll321@gmail.com>
2019-11-05 18:39:45 -0800

    Sending back, testing messed up due to how the cluster runs simultaneously.

--- P3/pleiades/execute.sh
+++ P3/pleiades/execute.sh
@@ -1,16 +1,67 @@
+#if [ -z "$1" ]
+#then
+#    sbatch -N1 ./hwrun.sh 2 | sort -V
+#    sbatch -N2 ./hwrun.sh 4 | sort -V
+#    sbatch -N4 ./hwrun.sh 8 | sort -V
+#    sbatch -N4 ./hwrun.sh 16 | sort -V
+#    sbatch -N8 ./hwrun.sh 32 | sort -V
+#    sbatch -N8 ./hwrun.sh 64 | sort -V
+# else
+#     sbatch -N1 ./hwrun.sh 2 150 74 49 49999 1024 | sort -V
+#     sbatch -N2 ./hwrun.sh 4 150 74 49 49999 1024 | sort -V
+#     sbatch -N4 ./hwrun.sh 8 150 74 49 49999 1024 | sort -V
+#     sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+#     sbatch -N8 ./hwrun.sh 32 150 74 49 49999 1024 | sort -V
+#     sbatch -N8 ./hwrun.sh 64 150 74 49 49999 1024 | sort -V
+# fi
+
+# p seed A B Prime n
+
 if [ -z "$1" ]
 then
-    sbatch -N1 ./hwrun.sh 2
-    sbatch -N2 ./hwrun.sh 4
-    sbatch -N4 ./hwrun.sh 8
-    sbatch -N4 ./hwrun.sh 16
-    sbatch -N8 ./hwrun.sh 32
-    sbatch -N8 ./hwrun.sh 64
+	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
 else
-    sbatch -N1 ./hwrun.sh 2 150 74 49 49999 1024
-    sbatch -N2 ./hwrun.sh 4 150 74 49 49999 1024
-    sbatch -N4 ./hwrun.sh 8 150 74 49 49999 1024
-    sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024
-    sbatch -N8 ./hwrun.sh 32 150 74 49 49999 1024
-    sbatch -N8 ./hwrun.sh 64 150 74 49 49999 1024
+	sbatch -N$1 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+fi
+
+if [ -z "$2" ]
+then
+	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+else
+	sbatch -N$1 ./hwrun.sh $2 150 74 49 49999 1024 | sort -V
+fi
+
+if [ -z "$3" ]
+then
+	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+else
+	sbatch -N$1 ./hwrun.sh $2 $3 74 49 49999 1024 | sort -V
+fi
+
+if [ -z "$4" ]
+then
+	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+else
+	sbatch -N$1 ./hwrun.sh $2 $3 $4 49 49999 1024 | sort -V
+fi
+
+if [ -z "$5" ]
+then
+	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+else
+	sbatch -N$1 ./hwrun.sh $2 $3 $4 $5 49999 1024 | sort -V
+fi
+
+if [ -z "$6" ]
+then
+	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+else
+	sbatch -N$1 ./hwrun.sh $2 $3 $4 $5 $6 1024 | sort -V
+fi
+
+if [ -z "$7" ]
+then
+	sbatch -N4 ./hwrun.sh 16 150 74 49 49999 1024 | sort -V
+else
+	sbatch -N$1 ./hwrun.sh $2 $3 $4 $5 $6 $7 | sort -V
 fi
