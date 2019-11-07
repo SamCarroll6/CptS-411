ebfb2e210da66aea30e7b353b761db8dbcdd0175
Sam Carroll <cfcarroll321@gmail.com>
2019-11-06 20:39:58 -0800

    Testing runtime with higher values.

--- P3/pleiades/Performancetesting/RT.sh
+++ P3/pleiades/Performancetesting/RT.sh
@@ -1,34 +1,34 @@
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 16
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 32
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 64
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 128
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 256
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 512
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 1024
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 2048
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 4096
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 8192
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 16384
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 32768
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 65536
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 131072
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 262144
-sleep 2
-sbatch -N4 ./hwrun.sh 16 150 74 49 4999 524288
-sleep 2
 sbatch -N4 ./hwrun.sh 16 150 74 49 4999 1048576
 sleep 2
+sbatch -N4 ./hwrun.sh 16 150 74 49 4999 2097152
+sleep 2
+sbatch -N4 ./hwrun.sh 16 150 74 49 4999 4194304
+sleep 2
+sbatch -N4 ./hwrun.sh 16 150 74 49 4999 8388608
+sleep 2
+sbatch -N4 ./hwrun.sh 16 150 74 49 4999 16777216
+sleep 2
+sbatch -N4 ./hwrun.sh 16 150 74 49 4999 33554432
+sleep 2
+sbatch -N4 ./hwrun.sh 16 150 74 49 4999 67108864
+#sleep 2
+# sbatch -N4 ./hwrun.sh 16 150 74 49 4999 2048
+# sleep 2
+# sbatch -N4 ./hwrun.sh 16 150 74 49 4999 4096
+# sleep 2
+# sbatch -N4 ./hwrun.sh 16 150 74 49 4999 8192
+# sleep 2
+# sbatch -N4 ./hwrun.sh 16 150 74 49 4999 16384
+# sleep 2
+# sbatch -N4 ./hwrun.sh 16 150 74 49 4999 32768
+# sleep 2
+# sbatch -N4 ./hwrun.sh 16 150 74 49 4999 65536
+# sleep 2
+# sbatch -N4 ./hwrun.sh 16 150 74 49 4999 131072
+# sleep 2
+# sbatch -N4 ./hwrun.sh 16 150 74 49 4999 262144
+# sleep 2
+# sbatch -N4 ./hwrun.sh 16 150 74 49 4999 524288
+# sleep 2
+# sbatch -N4 ./hwrun.sh 16 150 74 49 4999 1048576
+# sleep 2
