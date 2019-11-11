33fe756740fedd043df273fa837807ae3664eeae
Sam Carroll <cfcarroll321@gmail.com>
2019-11-10 18:50:43 -0800

    Had a lot messed up with my executables, hopefully this resolves it.

--- P4/pleiades/Prec.sh
+++ P4/pleiades/Prec.sh
@@ -1,18 +1,16 @@
-#!/bin/sh
+#! /bin/sh
 
-gcc -o main -fopenmp main.c -lm
-
-sbatch -N1 ./hwrun.sh 1000000 1
+./hwrun.sh 1000000 1
 sleep 2
-sbatch -N1 ./hwrun.sh 2000000 2
+./hwrun.sh 2000000 2
 sleep 2
-sbatch -N1 ./hwrun.sh 4000000 4
+./hwrun.sh 4000000 4
 sleep 2
-sbatch -N1 ./hwrun.sh 8000000 8
+./hwrun.sh 8000000 8
 sleep 2
-sbatch -N1 ./hwrun.sh 16000000 16
+./hwrun.sh 16000000 16
 sleep 2
-sbatch -N1 ./hwrun.sh 32000000 32
+./hwrun.sh 32000000 32
 sleep 2
-sbatch -N1 ./hwrun.sh 64000000 64
+./hwrun.sh 64000000 64
 
--- P4/pleiades/SU.sh
+++ P4/pleiades/SU.sh
@@ -1,24 +1,22 @@
-#!/bin/sh
-
-gcc -o main -fopenmp main.c -lm
+#! /bin/sh
 
 
 if [ -z "$2" ]
 then
-    sbatch -N1 ./hwrun.sh 1000000 1
+    ./hwrun.sh 1000000 1
     sleep 2
-    sbatch -N1 ./hwrun.sh 1000000 2
+    ./hwrun.sh 1000000 2
     sleep 2
-    sbatch -N1 ./hwrun.sh 1000000 4
+    ./hwrun.sh 1000000 4
     sleep 2
-    sbatch -N1 ./hwrun.sh 1000000 8
+    ./hwrun.sh 1000000 8
     sleep 2
 else
-    sbatch -N1 ./hwrun.sh $1 1
+    ./hwrun.sh $1 1
     sleep 2
-    sbatch -N1 ./hwrun.sh $1 2
+    ./hwrun.sh $1 2
     sleep 2
-    sbatch -N1 ./hwrun.sh $1 4
+    ./hwrun.sh $1 4
     sleep 2
-    sbatch -N1 ./hwrun.sh $1 8
+    ./hwrun.sh $1 8
 fi
