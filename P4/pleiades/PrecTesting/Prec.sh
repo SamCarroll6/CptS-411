99be3449c7a54409d479567fa994b7b0dc2c7814
Sam Carroll <cfcarroll321@gmail.com>
2019-11-12 19:13:34 -0800

    Changing precision testing.

--- P4/pleiades/PrecTesting/Prec.sh
+++ P4/pleiades/PrecTesting/Prec.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
-sbatch -N1 ./hwrun.sh 10000000 1
+sbatch -N1 ./hwrun.sh 20000000 1
 sleep 2
-sbatch -N1 ./hwrun.sh 20000000 2
+sbatch -N1 ./hwrun.sh 40000000 2
 sleep 2
-sbatch -N1 ./hwrun.sh 40000000 4
+sbatch -N1 ./hwrun.sh 80000000 4
 sleep 2
-sbatch -N1 ./hwrun.sh 80000000 8
+sbatch -N1 ./hwrun.sh 160000000 8
 sleep 2
-sbatch -N1 ./hwrun.sh 160000000 16
+sbatch -N1 ./hwrun.sh 320000000 16
 sleep 2
-sbatch -N1 ./hwrun.sh 320000000 32
+sbatch -N1 ./hwrun.sh 640000000 32
 sleep 2
-sbatch -N1 ./hwrun.sh 640000000 64
+sbatch -N1 ./hwrun.sh 1280000000 64
 
