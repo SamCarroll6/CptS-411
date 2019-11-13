5e0aaec29d5748f638d50fe246b1016621ca995a
Sam Carroll <cfcarroll321@gmail.com>
2019-11-13 07:18:33 -0800

    One last test.

--- P4/pleiades/PrecTesting/Prec.sh
+++ P4/pleiades/PrecTesting/Prec.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
-sbatch -N1 ./hwrun.sh 65536 1
+sbatch -N1 ./hwrun.sh 4194304 1
 sleep 2
-sbatch -N1 ./hwrun.sh 131072 2
+sbatch -N1 ./hwrun.sh 8388608 2
 sleep 2
-sbatch -N1 ./hwrun.sh 262144 4
+sbatch -N1 ./hwrun.sh 16777216 4
 sleep 2
-sbatch -N1 ./hwrun.sh 524288 8
+sbatch -N1 ./hwrun.sh 33554432 8
 sleep 2
-sbatch -N1 ./hwrun.sh 1048576 16
+sbatch -N1 ./hwrun.sh 67108864 16
 sleep 2
-sbatch -N1 ./hwrun.sh 32 32
+sbatch -N1 ./hwrun.sh 134217728 32
 sleep 2
-sbatch -N1 ./hwrun.sh 64 64
+sbatch -N1 ./hwrun.sh 268435456 64
 sleep 2
