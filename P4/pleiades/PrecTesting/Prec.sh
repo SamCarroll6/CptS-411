a0b9a40062dcfdd8ac69573e7a6e838b3bafc55d
Sam Carroll <cfcarroll321@gmail.com>
2019-11-12 21:52:56 -0800

    Sending over.

--- P4/pleiades/PrecTesting/Prec.sh
+++ P4/pleiades/PrecTesting/Prec.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
-sbatch -N1 ./hwrun.sh 2 1
+sbatch -N1 ./hwrun.sh 16 1
 sleep 2
-sbatch -N1 ./hwrun.sh 4 2
+sbatch -N1 ./hwrun.sh 32 2
 sleep 2
-sbatch -N1 ./hwrun.sh 8 4
+sbatch -N1 ./hwrun.sh 64 4
 sleep 2
-sbatch -N1 ./hwrun.sh 16 8
+sbatch -N1 ./hwrun.sh 128 8
 sleep 2
-sbatch -N1 ./hwrun.sh 32 16
+sbatch -N1 ./hwrun.sh 256 16
 sleep 2
-sbatch -N1 ./hwrun.sh 64 32
+sbatch -N1 ./hwrun.sh 512 32
 sleep 2
-sbatch -N1 ./hwrun.sh 128 64
+sbatch -N1 ./hwrun.sh 1024 64
 
