216eb12d342093862da82f218cb2a0374aad592e
Sam Carroll <cfcarroll321@gmail.com>
2019-11-12 21:47:37 -0800

    Testing one more precision.

--- P4/pleiades/PrecTesting/Prec.sh
+++ P4/pleiades/PrecTesting/Prec.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
-sbatch -N1 ./hwrun.sh 1024 1
+sbatch -N1 ./hwrun.sh 2 1
 sleep 2
-sbatch -N1 ./hwrun.sh 2048 2
+sbatch -N1 ./hwrun.sh 4 2
 sleep 2
-sbatch -N1 ./hwrun.sh 4096 4
+sbatch -N1 ./hwrun.sh 8 4
 sleep 2
-sbatch -N1 ./hwrun.sh 8192 8
+sbatch -N1 ./hwrun.sh 16 8
 sleep 2
-sbatch -N1 ./hwrun.sh 16384 16
+sbatch -N1 ./hwrun.sh 32 16
 sleep 2
-sbatch -N1 ./hwrun.sh 32768 32
+sbatch -N1 ./hwrun.sh 64 32
 sleep 2
-sbatch -N1 ./hwrun.sh 65536 64
+sbatch -N1 ./hwrun.sh 128 64
 
